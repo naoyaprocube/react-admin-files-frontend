4851ca938c8eb8485a59dab420399a11cb34fe10
naoyaprocube <naoya@procube.jp>
2023-05-12 14:18:11 +0900

    support Docker

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -1,7 +1,7 @@
 import { fetchUtils } from 'react-admin';
 import { stringify } from 'query-string';
 
-const apiUrl = 'http://localhost:3000/api';
+const apiUrl = 'https://localhost:19444/fileserver/api';
 const httpClient = fetchUtils.fetchJson;
 
 const dataProvider = {
